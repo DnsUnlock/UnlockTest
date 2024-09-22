d6b3b5f286971f98f4a208be2a19c8608f34cec5
DnsUnlock <code@dnsunlock.com>
2024-09-23 02:26:56 +0800

    更新chatgpt域名

--- testUnlock/ChatGPT.go
+++ testUnlock/ChatGPT.go
@@ -11,10 +11,10 @@ import (
 )
 
 func SupportGPT(loc string) bool {
-	var GPT_SUPPORT_COUNTRY = []string{
+	var GptSupportCountry = []string{
 		"AL", "DZ", "AD", "AO", "AG", "AR", "AM", "AU", "AT", "AZ", "BS", "BD", "BB", "BE", "BZ", "BJ", "BT", "BA", "BW", "BR", "BG", "BF", "CV", "CA", "CL", "CO", "KM", "CR", "HR", "CY", "DK", "DJ", "DM", "DO", "EC", "SV", "EE", "FJ", "FI", "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD", "GT", "GN", "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KW", "KG", "LV", "LB", "LS", "LR", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MR", "MU", "MX", "MC", "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ", "NI", "NE", "NG", "MK", "NO", "OM", "PK", "PW", "PA", "PG", "PE", "PH", "PL", "PT", "QA", "RO", "RW", "KN", "LC", "VC", "WS", "SM", "ST", "SN", "RS", "SC", "SL", "SG", "SK", "SI", "SB", "ZA", "ES", "LK", "SR", "SE", "CH", "TH", "TG", "TO", "TT", "TN", "TR", "TV", "UG", "AE", "US", "UY", "VU", "ZM", "BO", "BN", "CG", "CZ", "VA", "FM", "MD", "PS", "KR", "TW", "TZ", "TL", "GB",
 	}
-	for _, s := range GPT_SUPPORT_COUNTRY {
+	for _, s := range GptSupportCountry {
 		if loc == s {
 			return true
 		}
@@ -23,7 +23,7 @@ func SupportGPT(loc string) bool {
 }
 
 func ChatGPT(c http.Client) result.Result {
-	resp, err := url.GET(c, "https://chat.openai.com/cdn-cgi/trace")
+	resp, err := url.GET(c, "https://chatgpt.com/cdn-cgi/trace")
 	if err != nil {
 		return result.Result{Status: status.NetworkErr, Err: errs.Network}
 	}
@@ -44,7 +44,7 @@ func ChatGPT(c http.Client) result.Result {
 	}
 	loc := s[:i]
 
-	resp, err = url.GET(c, "https://chat.openai.com")
+	resp, err = url.GET(c, "https://chatgpt.com")
 	if err != nil {
 		return result.Result{Status: status.NetworkErr, Err: errs.Network}
 	}
